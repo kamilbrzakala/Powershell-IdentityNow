498ef6107b048f7be36ec47551061369c037c31f
kamilbrzakala <bzykson@gmail.com>
2023-01-18 10:22:15 +0100

    Update credentials-convert.ps1

--- credentials-convert.ps1
+++ credentials-convert.ps1
@@ -1,10 +1,15 @@
 cls
+## INSTRUCTION OF USE
+## 1. setup pwdIn by passing password to popup
+$pwdIn = read-host -assecurestring  | ConvertFrom-SecureString # | out-file $outFile
 
-#$pwdIn = read-host -assecurestring  | ConvertFrom-SecureString # | out-file $outFile
-
-# store hashed value in config file instead of plain password
-$pwdOut = "01000000d08c9dd............" | ConvertTo-SecureString
+#$pwdOut = "01000000d08c9dd............" | ConvertTo-SecureString
+$pwdOut = $pwdIn | ConvertTo-SecureString
 $pwdOut
 $creds = New-Object System.Management.Automation.PSCredential -ArgumentList "user", $pwdOut 
-$creds.GetNetworkCredential().Password
 
+# execute below if you wanna read unencrypted password (as plain text)
+#$creds.GetNetworkCredential().Password
+
+# use it as param in invoke
+Invoke-WebRequest -UseBasicParsing -Credential $creds -Uri
